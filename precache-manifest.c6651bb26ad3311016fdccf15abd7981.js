bf0d194377167b28396b542e06f9f4f6349ce01d
StefanLjiljak <stefanljiljak@gmail.com>
2020-06-18 20:42:26 +0200

    Updates

---
+++ precache-manifest.c6651bb26ad3311016fdccf15abd7981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480142412266c1029ddc17356c864d0b",
+    "url": "/easy-peasy-react-state-management/index.html"
+  },
+  {
+    "revision": "cfd0876c43adb546c20b",
+    "url": "/easy-peasy-react-state-management/static/css/main.e8d17a31.chunk.css"
+  },
+  {
+    "revision": "4cf59dfc0892f70bb753",
+    "url": "/easy-peasy-react-state-management/static/js/2.675bd675.chunk.js"
+  },
+  {
+    "revision": "fbdb25c6a30511ca0525a561a8e4c009",
+    "url": "/easy-peasy-react-state-management/static/js/2.675bd675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd0876c43adb546c20b",
+    "url": "/easy-peasy-react-state-management/static/js/main.7fbd7344.chunk.js"
+  },
+  {
+    "revision": "ff60ec4b6208de4afed4",
+    "url": "/easy-peasy-react-state-management/static/js/runtime-main.7b6f716e.js"
+  }
+]);
